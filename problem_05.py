459af887aa4f6e6e13b816111aa4891062a9dbcd
Sudip Bharati <sudipjungbharatii@gmail.com>
2026-04-14 16:37:08 +0545

    Add files via upload

---
+++ problem_05.py
@@ -0,0 +1,9 @@
+#5. Write a program which finds out whether a given name is present in a list or not.
+
+names = ["apple", "banana", "mango"]
+name = input("Enter name: ")
+
+if name in names:
+    print("Yes")
+else:
+    print("No")
