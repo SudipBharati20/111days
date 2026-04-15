66b5b55e5544eab82b42aa843afc69cb47d5fed5
Sudip Bharati <sudipjungbharatii@gmail.com>
2026-04-15 09:17:09 +0545

    Function

---
+++ function.py
@@ -0,0 +1,14 @@
+def percent(marks):    return sum(marks)/700*100
+
+marks = [23,45,65,6,4,56,64]
+percentage = percent(marks)
+
+marks2 = [55,66,7,6,7,67,74]
+percentage2 = percent(marks2)
+print(percentage,percentage2)
+
+# Write a program to greet a user with “Good day” using functions.
+def greet():
+    print("Good day!")
+
+greet()
