dda47924a3291bf3a2b68bff431630825e3c1e1c
Sudip Bharati <sudipjungbharatii@gmail.com>
2026-04-15 17:53:25 +0545

    Final cleanup of root directory

--- problem_02.py
+++
@@ -1,7 +0,0 @@
-#Write a program to greet all the person names stored in a list ‘l’ and which starts
-#with S.
-l1 = ["Sher", "Sunny", "Rahul", "Sakshi"]
-
-for name in l1:
-    if name.startswith("S"):
-        print(f"Hello, {name}!")
--- problem_05.py
+++
@@ -1,9 +0,0 @@
-#5. Write a program which finds out whether a given name is present in a list or not.
-
-names = ["apple", "banana", "mango"]
-name = input("Enter name: ")
-
-if name in names:
-    print("Yes")
-else:
-    print("No")
