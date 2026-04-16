74ce207d75aaf61bd81000774bc1a4ae0495a8f2
Sudip Bharati <sudipjungbharatii@gmail.com>
2026-04-16 18:28:56 +0545

    Add files via upload

---
+++ day 3/OOP/problem_01.py
@@ -0,0 +1,11 @@
+#Write a class “Calculator” capable of finding square, cube and square root of a 
+#number.
+class Calculator:
+    def square(self, num):
+        return num ** 2
+
+    def cube(self, num):
+        return num ** 3
+
+    def square_root(self, num):
+        return num ** 0.5
