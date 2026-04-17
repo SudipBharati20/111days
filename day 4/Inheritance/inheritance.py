6b35172550217c829855f7caa01de33bd4db879c
Sudip Bharati <sudipjungbharatii@gmail.com>
2026-04-17 20:19:58 +0545

    Add files via upload

---
+++ day 4/Inheritance/inheritance.py
@@ -0,0 +1,8 @@
+#inheritance
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def display(self):
+        print(f"Name: {self.name}, Age: {self.age}")
