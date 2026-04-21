a15cb52934a9262e453e34b3cba9d44eae096a9e
Sudip Bharati <sudipjungbharatii@gmail.com>
2026-04-21 19:29:29 +0545

    Add files via upload

---
+++ day 8 advance python 2/musicLibrary.py
@@ -0,0 +1,9 @@
+
+# File: musicLibrary.py
+# Stores music links
+
+music = {
+    "shapeofyou": "https://www.youtube.com/watch?v=JGwWNGJdvx8",
+    "believer": "https://www.youtube.com/watch?v=7wtfhZwyrcc",
+    "despacito": "https://www.youtube.com/watch?v=kJQP7kiw5Fk"
+}
